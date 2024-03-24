60808f45924cb65b06da8cf0fe792d4c5a53a5c5
Raddames <raddamestonui48@gmail.com>
2024-03-24 22:58:35 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    console.log(customerName.toUpperCase()); 
+    
+}
+
+function setBestCustomer() {
+    
+    
+}
